5347f9030cf10fb256b1ebded990bd4bfbcf305e
pilillo <>
2021-03-02 22:59:39 +0100

    updated build script

--- build_target.sh
+++ build_target.sh
@@ -16,9 +16,9 @@ elif [ -d "${TARGETS_DIR}/${1}" ]; then
   echo "Building target ${1}"
   BUILD_TAG=$(date +%Y%m%d)
   docker build -t ${2}/${PROJECT}-${1}:${BUILD_TAG} -f ${TARGETS_DIR}/${1}/Dockerfile .
-  [ "${3}" == "push" ] && docker push ${2}/${PROJECT}:${BUILD_TAG}
+  [ "${3}" == "push" ] && docker push ${2}/${PROJECT}-${1}:${BUILD_TAG}
   docker image tag ${2}/${PROJECT}-${1}:${BUILD_TAG} ${2}/${PROJECT}-${1}:latest
-  [ "${3}" == "push" ] && docker push ${2}/${PROJECT}:latest
+  [ "${3}" == "push" ] && docker push ${2}/${PROJECT}-${1}:latest
 else
   echo "Selected target path ${1} does not exist"
   echo "Available targets: ${TARGETS[@]} "
